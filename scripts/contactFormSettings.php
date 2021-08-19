50b84938fc5908b7d73467c38226d23a0f25811c
StR0vErBG <44778089+StR0vErBG@users.noreply.github.com>
2021-08-19 03:24:31 -0700

    Add files via upload

---
+++ scripts/contactFormSettings.php
@@ -0,0 +1,12 @@
+<?php
+$email_to = "info@remontirapokrivi.com"; // your email address
+$email_subject = "Contact Form Message"; // email subject line
+$sendPage = "sendPage.html"; // thank you page
+
+// if you update the question on the form -
+// you need to update the questions answer below
+
+$antispam_answer = "25";
+
+$base = "CgpGb3JtIHBvd2VyZWQgYnkgaHR0cHM6Ly93d3cuZnJlZWNvbnRhY3Rmb3JtLmNvbQ==";
+?>
