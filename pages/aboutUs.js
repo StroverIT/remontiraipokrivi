1fa21899a22c6330bb274b91284ff41d32253f93
StR0vErBG <xcreaw1@abv.bg>
2023-09-23 10:38:56 +0200

    removing about us

--- pages/aboutUs.js
+++ pages/aboutUs.js
@@ -154,49 +154,7 @@ export default function AboutUs() {
               </div>
             </div> */}
           </section>
-          <section className="flex-col py-16 flex-center ">
-            <div className="flex-center">
-              <Title title="ЕКИПЪТ" size="text-4xl" />
-            </div>
-            <section className="grid mt-16 md:grid-cols-3 gap-x-20 gap-y-10">
-              <div className="flex-col flex-center">
-                <div className="relative transition-all border-2 rounded-full cursor-pointer w-36 h-36 hover:scale-110 border-primaryBlue-100 ">
-                  {/* <Image
-                    src="/djulia.jpg"
-                    alt="growth for you and your company"
-                    layout="fill"
-                    className="object-contain rounded-full "
-                  /> */}
-                </div>
-                <h2 className="mt-3 text-xl text-center">Денислав Цветков</h2>
-                <p>Управител</p>
-              </div>
-              <div className="flex-col flex-center">
-                <div className="relative transition-all border-2 rounded-full cursor-pointer w-36 h-36 hover:scale-110 border-primaryBlue-100 ">
-                  {/* <Image
-                    src="/hrisi2_durt_crop.jpg"
-                    alt="growth for you and your company"
-                    layout="fill"
-                    className="rounded-full bg-blend"
-                  /> */}
-                </div>
-                <h2 className="mt-3 text-xl text-center">Йоана Стайкова</h2>
-                <p>Hr консултант</p>
-              </div>
-              <div className="flex-col flex-center">
-                <div className="relative transition-all border-2 rounded-full cursor-pointer w-36 h-36 hover:scale-110 border-primaryBlue-100 ">
-                  <Image
-                    src="/personal/test_v3.jpg"
-                    alt="growth for you and your company"
-                    layout="fill"
-                    className="object-contain rounded-full bg-[#afafaf]"
-                  />
-                </div>
-                <h2 className="mt-3 text-xl text-center">Емил Златинов</h2>
-                <p>Ит съпорт</p>
-              </div>
-            </section>
-          </section>
+       
           <section className="pb-20">
             <ContactUs
               color="border-blue"
